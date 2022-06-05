04c6bdcf9042e7eb14e3d0bd63c7df683cca36ef
Sodiq179 <babawalesodiq996@gmail.com>
2022-06-05 22:27:04 +0000

    initial commit

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+sible_by_2(my_list=[]):
+    if my_list:
+        values = []
+        for i in my_list:
+            if i % 2 is 0:
+                values.append(True)
+            else:
+                values.append(False)
+        return values
