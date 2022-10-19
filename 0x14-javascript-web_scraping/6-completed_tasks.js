672a22a6790b0b98c7a1e01c456c23fd9026b3b5
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-19 11:20:48 +0000

    Initial commit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,10 +5,14 @@ const API_URL = process.argv[2];
 
 request(API_URL, function (error, response, body) {
 	let content = JSON.parse(body);
-	let userCount = {"1":0, "2":0, "3":0, "4":0, "5":0, "6":0, "7":0, "8":0, "9":0, "10":0}
+	let userCount = {}
 	let usercount = content.length;
 	for (i = 0; i < usercount; i++){
-		if (content[i]["completed"]){
+		if (!userCount.hasOwnProperty(content[i]["userId"].toString())){
+			userCount[content[i]["userId"].toString()] = 0;
+		}
+		else if (userCount.hasOwnProperty(content[i]["userId"].toString())
+ && content[i]["completed"]){
 			userCount[content[i]["userId"].toString()] += 1
 		}
 	}
